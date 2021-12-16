65c4028a4603e6275c2c8c29a6b9b6f5c69e72ae
trufstevghac <96149272+trufstevghac@users.noreply.github.com>
2021-12-16 13:20:30 -0500

    Changing it back to the normal entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,14 +1,14 @@
 #! /usr/bin/env bash
 
 args=("$@")
-#/usr/local/bin/launcher git ${args[@]}
-echo ${args[@]}
+/usr/local/bin/launcher git ${args[@]}
+#echo ${args[@]}
 
-apt-get update
-apt-get install git -y
+#apt-get update
+#apt-get install git -y
 
 #which git
 #git branch --all
 #git tag -l
-git show-ref
-git reflog
+#git show-ref
+#git reflog
