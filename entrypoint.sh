5abce7a262c5ff9354aaa1a924c047c6a41248d6
trufstevghac <96149272+trufstevghac@users.noreply.github.com>
2021-12-16 13:12:09 -0500

    Look at git reflog and show-ref

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,9 +4,11 @@ args=("$@")
 #/usr/local/bin/launcher git ${args[@]}
 echo ${args[@]}
 
-#apt-get update
-#apt-get install git -y
+apt-get update
+apt-get install git -y
 
 #which git
 #git branch --all
 #git tag -l
+git show-ref
+git reflog
